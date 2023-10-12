b538251170a3f96d9a68487f99b7be784062745f
radhikab1 <radhika3663@gmail.com>
2023-10-11 22:55:08 -0700

    connected to database via mongoDB atlas & mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // express app
@@ -19,7 +20,15 @@ app.use('/api/workouts', workoutRoutes)
 //     res.json({mssg: 'Welcome to the app'})
 // })
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
