9eb7c82f21cfe5cab516b9b51c4eb2d84652e158
radhikab1 <radhika3663@gmail.com>
2023-10-16 22:05:54 -0700

    added react form for new workout

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 
 // components
 import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from '../components/WorkoutForm'
 
 const Home = () => {
     const [workouts, setWorkouts] = useState(null)
@@ -21,11 +22,12 @@ const Home = () => {
 
     return (
         <div className="home">
-            <div className="wokouts">
+            <div className="workouts">
                 {workouts && workouts.map((workout) => (
                     <WorkoutDetails key={workout._id} workout={workout}/>
                 ))}
             </div>
+            <WorkoutForm/>
         </div>
     )
 }
